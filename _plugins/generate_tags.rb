73ad33c1989b3267492bca06b5498fc680fd9318
LuisSevillano <luissevillanopires@gmail.com>
2024-05-04 01:55:22 +0200

    Remove empty characters

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -3,18 +3,13 @@ module Jekyll
     safe true
 
     def generate(site)
-      # Leer archivo YAML
       works_data = YAML.load_file("_data/works.yml")
 
-      # Crear una lista de todos los tags sin duplicados
       all_tags = works_data.flat_map { |work| work['tags'] }.uniq
 
-      # Iterar sobre cada tag
       all_tags.each do |tag|
-        # Crear una lista de elementos que contienen el tag actual
         tagged_elements = works_data.select { |work| work['tags'] && work['tags'].include?(tag) }
 
-        # Generar la página para el tag actual
         site.pages << TagPage.new(site, tag, tagged_elements)
       end
     end
@@ -29,7 +24,7 @@ module Jekyll
 
       self.process(@name)
       self.data = {
-        'layout' => 'tags', # Layout específico para las páginas de tag
+        'layout' => 'tags',
         'title' => "Posts tagged as #{tag}",
         'tag' => tag,
         'tagged_elements' => tagged_elements
