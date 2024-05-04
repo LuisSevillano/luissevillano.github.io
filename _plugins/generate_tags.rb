3aebfb179efbd818fb80cb846a9e6c947075612c
LuisSevillano <luissevillanopires@gmail.com>
2024-05-04 02:48:28 +0200

    YAML data path

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -3,46 +3,31 @@ module Jekyll
     safe true
 
     def generate(site)
-      tag_mapping = {}
+      works_data = YAML.load_file("./_data/works.yml")
 
-      tags_posts = {}
+      all_tags = works_data.flat_map { |work| work['tags'] }.uniq
 
-      site.posts.docs.each do |post|
-        next unless post.published?
-        post.data['tags'].each do |tag|
-          normalized_tag = tag.downcase.gsub(' ', '_')
+      all_tags.each do |tag|
+        tagged_elements = works_data.select { |work| work['tags'] && work['tags'].include?(tag) }
 
-          tag_mapping[tag] = normalized_tag
-
-          tags_posts[normalized_tag] ||= []
-          tags_posts[normalized_tag] << post
-        end
-      end
-
-      site.data['tag_mapping'] = tag_mapping
-
-      tags_posts.each do |normalized_tag, posts|
-        if normalized_tag
-          site.pages << TagPage.new(site, tag_mapping.key(normalized_tag), normalized_tag, posts)
-        end
+        site.pages << TagPage.new(site, tag, tagged_elements)
       end
     end
   end
 
   class TagPage < Page
-    def initialize(site, tag, normalized_tag, posts)
+    def initialize(site, tag, tagged_elements)
       @site = site
       @base = site.source
-      @dir = File.join('tags', normalized_tag)
+      @dir = File.join('tags', tag.downcase.gsub(' ', '_'))
       @name = 'index.html'
 
       self.process(@name)
       self.data = {
         'layout' => 'tags',
         'title' => "Posts tagged as #{tag}",
-        'original_tag' => tag,
-        'normalized_tag' => normalized_tag,
-        'posts' => posts
+        'tag' => tag,
+        'tagged_elements' => tagged_elements
       }
     end
   end
